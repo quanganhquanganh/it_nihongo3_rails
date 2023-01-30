69ae09959da1e7dcc9610c0f9df240941952b75d
quanganhquanganh <quanganhdb2510@gmail.com>
2023-01-30 13:04:44 +0700

    Add avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   extend Devise::Models
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :avatar
 end
