abb767efed4548e4f11d2e399bb99e8b48139486
quanganhquanganh <quanganhdb2510@gmail.com>
2023-01-30 12:01:39 +0700

    Transition to Books

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-  devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :books
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
